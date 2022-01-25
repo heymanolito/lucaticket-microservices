b3cbff4f0714ed8fb5f21c961c9c65911ff5c5bf
ALUMNO16\alumno <subirats2000@gmail.com>
2022-01-25 12:45:29 +0100

    LP3-30/32

--- event-service/src/main/java/com/grupo1/lucaticket/repository/EventRepository.java
+++ event-service/src/main/java/com/grupo1/lucaticket/repository/EventRepository.java
@@ -2,6 +2,8 @@ package com.grupo1.lucaticket.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository{
+import com.grupo1.lucaticket.model.Event;
+
+public interface EventRepository extends JpaRepository<Event, Long>{
 
 }
--- event-service/src/main/java/com/grupo1/lucaticket/service/EventService.java
+++ event-service/src/main/java/com/grupo1/lucaticket/service/EventService.java
@@ -1,5 +1,10 @@
 package com.grupo1.lucaticket.service;
 
+import java.util.List;
+
+import com.grupo1.lucaticket.model.Event;
+
 public interface EventService {
 
+	List<Event> findAll();
 }
