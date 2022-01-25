030c0736ae85051fb6cb484156ed1cbe59b50191
heymanolito <manuelgallardoffs@gmail.com>
2022-01-25 14:07:08 +0100

    Eureka Server

---
+++ eureka-server/src/main/java/com/grupo1/lucaticket/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.grupo1.lucaticket.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+/**
+ * Eureka Annoation
+ */
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
