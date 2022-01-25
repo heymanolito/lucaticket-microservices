939d5cf2bcdd9a41e025fab762cd5002d08ca5b5
heymanolito <manuelgallardoffs@gmail.com>
2022-01-25 10:26:52 +0100

    Add EventResponse class to dto package.

--- event-service/src/main/java/com/grupo1/lucaticket/dto/EventResponse.java
+++ event-service/src/main/java/com/grupo1/lucaticket/dto/EventResponse.java
@@ -1,5 +1,23 @@
 package com.grupo1.lucaticket.dto;
 
-public class EventResponse {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class EventResponse implements Serializable {
+
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private String nombre;
+    private LocalDate fechaEvento;
+    private LocalDate horaEvento;
+    private String nombreRecinto;
+    private String ciudadRecinto;
 
 }
