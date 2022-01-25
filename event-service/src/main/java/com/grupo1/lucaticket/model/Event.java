184775caec54fbc96f7fe667a49a8ad957bd5bc0
ALUMNO02\alumno <n.borregolopez@gmail.com>
2022-01-25 10:36:18 +0100

    probando mongo

--- event-service/src/main/java/com/grupo1/lucaticket/model/Event.java
+++ event-service/src/main/java/com/grupo1/lucaticket/model/Event.java
@@ -2,4 +2,14 @@ package com.grupo1.lucaticket.model;
 
 public class Event {
 
+	// @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Event[id=%s, nombre='%s', descripcionCorta='%s', descripcionExtendida='%s', foto='%s', fechaEvento='%s', fechaEvento='%s',  "
+				
+				)
+				
+	}
 }
