e67733ac31a9997a8bac7bf5e22d37437a729978
ALUMNO16\alumno <subirats2000@gmail.com>
2022-01-25 10:54:29 +0100

    LP3-53

--- event-service/src/main/java/com/grupo1/lucaticket/model/Recinto.java
+++ event-service/src/main/java/com/grupo1/lucaticket/model/Recinto.java
@@ -1,5 +1,15 @@
 package com.grupo1.lucaticket.model;
 
-public class Recinto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+public @Data @AllArgsConstructor @NoArgsConstructor class Recinto {
+	
+	private Long id;
+	private String nombre;
+	private String ciudad;
+	private String direccion;
+	private Integer aforo;
+	
 }
