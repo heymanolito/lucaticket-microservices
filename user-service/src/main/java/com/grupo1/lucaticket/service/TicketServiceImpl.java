51f4dfd391e439f78866c334904cb6768e436893
ALUMNO16\alumno <subirats2000@gmail.com>
2022-02-03 19:38:38 +0100

    SDSad

--- user-service/src/main/java/com/grupo1/lucaticket/service/TicketServiceImpl.java
+++ user-service/src/main/java/com/grupo1/lucaticket/service/TicketServiceImpl.java
@@ -1,30 +1,34 @@
 package com.grupo1.lucaticket.service;
 
-import com.grupo1.lucaticket.dto.RequestEventDto;
-import com.grupo1.lucaticket.dto.ResponseEventDto;
-import com.grupo1.lucaticket.model.Ticket;
-import com.grupo1.lucaticket.repository.TicketRepository;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.grupo1.lucaticket.dto.RequestEventDto;
+import com.grupo1.lucaticket.dto.ResponseEventDto;
+import com.grupo1.lucaticket.model.Ticket;
+import com.grupo1.lucaticket.repository.TicketRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class TicketServiceImpl implements TicketService {
 
-    private static final Logger log = LoggerFactory.getLogger(TicketServiceImpl.class);
     @Autowired
     private TicketRepository repository;
+
     @Autowired
     private UserEntityService userService;
 
+    private static final Logger log = LoggerFactory.getLogger(TicketServiceImpl.class);
+
     public ResponseEventDto saveTicket(RequestEventDto eventDto) {
-        log.info("---[SERVICE] GUARDANDO TICKET..." + repository.save(convertirTicket(eventDto)));
-        return ResponseEventDto.builder()
+       log.info("---[SERVICE] GUARDANDO TICKET..." + repository.save(convertirTicket(eventDto)));
+       return ResponseEventDto.builder()
                 .nombreEvento(eventDto.getNombreEvento())
                 .precioEvento(eventDto.getPrecioEvento())
                 .message(eventDto.getMessage()).build();
